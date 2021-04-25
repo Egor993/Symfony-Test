2c58c6aa95c5372efea91bfd05ea3508bda017eb
Egor Cherepanov <feda9938765@yandex.ru>
2021-04-25 15:11:30 +0300

    #0 Symfony initialization

---
+++ app/src/Controller/LuckyController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\TestFormType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use App\Entity\Authors;
+use App\Entity\Books;
+use App\Services\FormAuthorService;
+
+class LuckyController extends AbstractController
+{
+	 /**
+     * @Route("/", name="index")
+     */
+    public function number()
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+}
